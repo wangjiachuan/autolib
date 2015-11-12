b9c641d00ddfe32fde489fd16358cc5bfae3a85a
wangjiachuan <93126721@qq.com>
2015-11-12 15:49:59 +0800

    Update mail_service.py
    update syntex error

--- mail_service.py
+++ mail_service.py
@@ -3,8 +3,8 @@ from mandrill_send_email import *
 
 class MailService(object):
     name = 'mails'
-	
-	def __init__(self):
+    
+    def __init__(self):
         self.to=' '
         self.subject ='pay mail confirmation'
         self.msg =' '
@@ -17,19 +17,20 @@ class MailService(object):
         :param payload: validated data
         """
         
-		# extract the email element from payload
-		self.to=payload['client'][email]
+	# extract the email element from payload
+	self.to=payload['client'][email]
         self.msg =""""
 		    Dear {payee},
-			You have received a payment of {amout} {currency} from {client} {email}
-			yours
-			student
+	            You have received a payment of {amout} {currency} from {client} {email}
+		    yours
+		    student
 		""".format(payee=payload[][],amout=payload['payment'][amount],currency=payload['payment'][amount],client=payload['payment'][amount],email=payload['payment'][amount]) 
 		
-		# send the email out
-		try:
-            self.senderMandrill.send(self.subject,self.to, self.msg)
+	# send the email out
+	try:
+		self.senderMandrill.send(self.subject,self.to, self.msg)
         except:
+        	pass
 s
 			
 			
